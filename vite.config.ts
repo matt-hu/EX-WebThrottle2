d3bed4abf27dac0dccea9712a22ccacf8c861a7a
matt-hu <matt.hu1@outlook.com>
2022-11-01 18:14:10 +0000

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // Utilities
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import { fileURLToPath, URL } from 'node:url'
+
+import GithubActionsReporter from 'vitest-github-actions-reporter'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,4 +33,12 @@ export default defineConfig({
     ],
   },
   base: process.env.GH_BUILD ? '/EX-WebThrottle2/' : '/',
+  test: {
+    reporters: 
+              process.env.GITHUB_ACTIONS
+              ? ['default', new GithubActionsReporter()]
+              // ? ['html']
+              : ['default']
+
+  }
 })
