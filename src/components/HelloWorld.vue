5904606c705d0b72f6f4641364219b42fc5a7aaa
matt-hu <matt.hu1@outlook.com>
2022-11-01 19:14:49 +0000

    Test coverage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 const props = defineProps<{
-  msg: string
+  msg: string,
+  msg2: string,
 }>();
 </script>
 
 <template>
     <h1>{{ props.msg }}</h1>
+    <h2>{{ props.msg2 }}</h2>
 </template>
